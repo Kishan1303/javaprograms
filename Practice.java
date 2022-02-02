89a2407531f7b8acda562f64e7cfd0551e16b4ed
Kishan1303 <90850895+Kishan1303@users.noreply.github.com>
2022-02-02 19:13:55 +0530

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,40 @@
+Program 2 
+Class kb{
+   Int a ,b;
+String s;
+    Void arithmetic (int x , int y , int z)
+{
+A = X:
+B =y:
+S = z;
+    Switch (z)
+{
+    Case "+":
+     C=a+b;
+      System.out.println(c);
+    Case "-":
+     C=a-b;
+      System.out.println(c);
+
+    Case "/":
+     C=a/b;
+      System.out.println(c);
+
+    Case "*":
+     C=a*b;
+      System.out.println(c);
+}
+       
+} 
+Public static void main(String args[])
+{
+       Scanner scan = new scanner (System.in);
+      a = scan.nextInt();
+b = "";
+S= scan.next();
+
+
+      
+}
+
+}
